f6d137fb76bc7a44e30f9850df8e46c51cc16c6b
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2020-09-12 12:57:20 -0400

    moar bug fix

--- endpoints/event.js
+++ endpoints/event.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const parse = require('csv-parse/lib/sync');
 
 const keys = require('../private/keys.json');
+const datetime = require('../lib/datetime.js');
 
 function getOne(arr) {
     return arr[Math.floor(Math.random()*arr.length)];
@@ -12,10 +13,11 @@ module.exports.getChartDataStockPick = async function(ticker, startDay) {
     console.log(key);
     let response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker.replace('.', '-')}&interval=60min&outputsize=full&apikey=${key}&datatype=csv`);
     return response.text().then(async(text) => {
-        let startDate = (new Date(startDay)).getTime();
+        let year = startDay.split('-')[0], month=parseInt(startDay.split('-')[1])-1, day = startDay.split('-')[2];
+        let startDate = Date.UTC(year, month, day, 0, 0, 0);
         try { var json = await parse(text, {
             on_record: (record, {lines}) => {
-                record = lines===1 ? null : {t: record[0], y: record[4]};
+                record = lines===1 ? null : {t: datetime.parseDashedString(record[0]), y: record[4]};
                 if(record && parseInt(record.t) <= startDate) { return null; }
                 return record;
             },
@@ -28,9 +30,7 @@ module.exports.getChartDataStockPick = async function(ticker, startDay) {
             let latestAlphavantageDate = json[0] ? json[0].t : 0;
             for(const obj of iexJson) {
                 if(!obj.close) { continue; }
-                let year = obj.date.split('-')[0], month=parseInt(obj.date.split('-')[1])-1, day = obj.date.split('-')[2];
-                let hour = obj.minute.split(':')[0], minute = obj.minute.split(':')[1];
-                let entry = {t: Date.UTC(year, month, day, hour, minute, 0), y: obj.close};
+                let entry = {t: datetime.parseDashedString(`${obj.date} ${obj.minute}`), y: obj.close};
                 if(parseInt(entry.t) >= latestAlphavantageDate) {
                     json.unshift(entry);
                 }
